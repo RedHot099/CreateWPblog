e18a1f44ab08f9effe40bcb69872ca082f71a0b2
Jakub Eichner <s478874@st.amu.edu.pl>
2023-05-16 10:23:54 +0200

    Upload & unpack WP to FTP

---
+++ ftp.py
@@ -0,0 +1,61 @@
+from ftplib import FTP
+import requests
+
+class UploadFTP:
+    def __init__(self, \
+                host:str, \
+                name:str, \
+                password:str \
+                ) -> None:
+        
+        self.do_ftp(host, name, password)
+
+
+    def read(self, path):
+        with open(path, "r") as f:
+            return f.read().split('\n')
+        
+
+    def dir_del_r(self, server, dir):
+        for i in server.nlst(dir):
+            if(not i.endswith('.')):
+                print("deleted", i)
+                try:
+                    server.delete(i)
+                except Exception:
+                    self.dir_del_r(server, i)
+                    server.rmd(i)
+
+
+    def do_ftp(self, h, u, p):
+        with FTP(h, u, p) as server:
+            server.cwd('public_html')
+            #remove files from FTP (recursively)
+            if not 'dodaj_tagi_artur_v1.php' in server.nlst():
+                self.dir_del_r(server, '')
+
+                #upload files to FTP
+                file = open('C:/Users/Kuba/Documents/praca/Zaplecza/pliki-artur/wordpress-artur.zip', 'rb')
+                server.storbinary('STOR wordpress-artur.zip', file)
+                file.close()
+                file = open('C:/Users/Kuba/Documents/praca/Zaplecza/pliki-artur/wypakuj.php', 'rb')
+                server.storbinary('STOR wypakuj.php', file)
+                file.close()
+
+                #ping to unpack .zip
+                self.ping_unpack(h)
+            else:
+                print("Wordpress already on FTP")
+
+            
+            #clean trash afterwards
+            if 'wordpress-artur.zip' in server.nlst(): server.delete('wordpress-artur.zip')
+            if 'wypakuj.php' in server.nlst(): server.delete('wypakuj.php')
+
+
+    def ping_unpack(self, url):
+        requests.get("http://" + url + "/wypakuj.php")
+
+
+if __name__ == "__main__":
+    z = UploadFTP()
