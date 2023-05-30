5744890ca6560da6f777e6857bbc321bc4522fbe
Jakub Eichner <s478874@st.amu.edu.pl>
2023-05-30 09:08:17 +0200

    New runtime schedule

--- run.py
+++ run.py
@@ -8,46 +8,45 @@ from openai_article import OpenAI_article
 
 with open("credentials.json", "r") as creds:
 		credentials = json.load(creds)
-                
-
-create = Create(credentials["vd20"])
-
-domain_topics = create.do_stuff("input.txt")
-
-data = []
-
-with open("results.tsv", "r") as results:
-    data = [r.split("\t") for r in results.read().split("\n")]
+            
 
 
-for row, topic in zip(data,domain_topics):
-    if row == ['']:
-        pass
-    print(f"======={row[0]}=======")
+with open("input.txt", "r") as f:
+    domains = f.read().split('\n')
+
+create = Create(credentials["vd07"])
+
+
+for domain in domains:
+    print(f"======={domain}=======")
+    db_user, db_pass, ftp_user, ftp_pass = create.do_stuff(domain)
+
     print("Connecting to FTP")
-    f = UploadFTP(row[0], row[3], row[4], "C:/Users/Kuba/Documents/praca/Zaplecza/pliki-artur")
-    print("Initilazing WP setup")
-    wp = Setup_WP(row[0])
+    f = UploadFTP(domain, ftp_user, ftp_pass, "C:/Users/Kuba/Documents/praca/Zaplecza/pliki-artur")
 
-    uname, pwd = wp.install(row[1], row[2], row[0].partition(".")[0])
+    print("Initilazing WP setup")
+    wp = Setup_WP(domain)
+
+    uname, pwd = wp.install(db_user, db_pass, domain.partition(".")[0])
     with open("credentials.tsv", "a+") as output:
-        output.write(f"{row[0]}\t{uname}\t{pwd}\n")
+        output.write(f"{domain}\t{uname}\t{pwd}\n")
     print("Tweaking WP options")
     wp.setup(uname, pwd)
+
     """
     print("Getting API key")
-    wp = Setup_WP(row[0])
+    wp = Setup_WP(domain)
     api_key = wp.get_api_key(uname, pwd)
 
     openai = OpenAI_article(
           api_key=api_key, 
-          domain_name=row[0],
+          domain_name=domain,
           wp_login=uname,
           wp_pass=pwd
     )
 
     if topic == '':
-        input(f"O jakiej tematyce pisać artykuły na strone {row[0]}")
+        input(f"O jakiej tematyce pisać artykuły na strone {domain}")
 
     openai.create_structure(
           topic=topic,
