5fafd93ca7f45f7552899474630e48459213e510
RedHot099 <redhotdevil996@gmail.com>
2023-08-04 14:21:27 +0200

    Decent paralellization

--- openai_article.py
+++ openai_article.py
@@ -9,105 +9,125 @@ from wp_api import WP_API
 
 
 class OpenAI_article(OpenAI_API, WP_API):
-    def __init__(self) -> None:
+    def __init__(self, 
+                api_key, 
+                domain_name, 
+                wp_login, 
+                wp_pass,
+                lang:str = None
+                 ) -> None:
+        
         self.total_tokens = 0
+
+        OpenAI_API.__init__(self, api_key, lang)
+        WP_API.__init__(self, domain_name, wp_login, wp_pass)
+
 
     def download_img(self, img_prompt, img_path):
         urllib.request.urlretrieve(self.create_img(img_prompt), img_path)
         return img_path
+        
+
+    def shift_date(self):
+        self.start_date = self.start_date + timedelta(days=self.days_delta) if self.forward_delta else self.start_date - timedelta(days=self.days_delta)
+        return 1
 
 
     def create_article(self, 
                        header_num:int, 
                        title:str, 
                        publish_date: datetime, 
-                       cat_id:int = 1
-                       ):
+                       cat_id:int = 1,
+                       parallel:bool = False
+                       ) -> (str, int):
         headers, img_prompt = self.create_headers(title,header_num)
         text = ""
-        print("\tWriting sub-sections")
-        func = partial(self.write_paragraph, title)
-        with Pool() as pool:
-            for header, p in pool.imap(func, headers):
-                print(f"\t\tWrote section - {header}")
-                text += '<h2>'+header+'</h2>'+p
 
-        # for i, h in enumerate(headers):
-        #     if h != '':
-        #         print(f"\t\t{i+1}/{header_num}: {h}")
-        #         p = self.write_paragraph(h, title)
-        #         text += '<h2>'+h+'</h2>'+p
-        #         i -= 1
+        if parallel:
+            func = partial(self.write_paragraph, title)
+            with Pool() as pool:
+                for header, p in pool.imap(func, headers):
+                    print(f"\t\tWrote section - {header}")
+                    text += '<h2>'+header+'</h2>'+p
+        else:
+            for i, h in enumerate(headers):
+                if h != '':
+                    print(f"{cat_id}. {title}\t{i+1}/{header_num}: {h}")
+                    p = self.write_paragraph(h, title)
+                    text += '<h2>'+h+'</h2>'+p
+                    i -= 1
         
-        print(f"\tCreating article description")
         desc = self.write_description(text)
-        
-        print(f"\tCreateing image: {img_prompt}")
         if img_prompt != "":
-            img = self.download_img(img_prompt, ".imgs/test_photo.webp")
+            img = self.download_img(img_prompt, f".imgs/test_photo{datetime.now().microsecond}.webp")
             
             img_id = self.upload_img(img)
             os.remove(img)
 
-        print("\tUploading article")
         print(self.post_article(title, text, desc, img_id, publish_date, cat_id)['link'])
-
+        self.shift_date()
         print(f"Total tokens used: {self.total_tokens}")
+        return title, cat_id
+    
 
-
-    def publish_articles(self, 
-                         topic:str, 
-                         article_num:int, 
-                         header_num:int, 
-                         start_date: datetime, 
-                         days_delta:int = 7, 
-                         forward_delta:bool = True, 
-                         cat_id:int = 1
-                         ):
-
-        print(f"Creating {article_num} articles on topic {topic} - each containg {header_num} sub-sections")
-        titles = self.create_titles(topic, article_num)
-        
-        for i, title in enumerate(titles):
-            print(f"Article {i+1}/{article_num}: {title}")
-            self.create_article(header_num, title, start_date, cat_id)
-
-            start_date = start_date + timedelta(days=days_delta) if forward_delta else start_date - timedelta(days=days_delta)
-
-        return start_date
+    def new_category(self, cat:str, parent_id:int = None) -> int:
+        cat_desc = self.write_cat_description(cat)
+        cat_json = self.create_category(cat, cat_desc, parent_id)
+        cat_id = cat_json['id']
+        return cat, int(cat_id)
 
 
     def create_structure(self, 
                          topic:str, 
                          category_num:int, 
-                         subcategory_num:int, 
-                         article_num:int, 
-                         header_num:int, 
-                         days_delta:int = 7, 
-                         forward_delta:bool = True
+                         subcategory_num:int
                          ):
-        
-        start_date = datetime.now()
+        #create categories according to site topic
         categories = self.create_categories(topic, category_num)
         print(f"Created categories: {categories}")
 
-        for cat in categories:
-            cat_desc = self.write_cat_description(cat)
-            cat_json = self.create_category(cat, cat_desc)
-            cat_id = cat_json['id']
-            print(f"Adding articles to category {cat_id}: {cat_json['link']}")
+        with Pool() as pool:
+            #paralelly for each category create description & subcategories
+            for cat, cat_id in pool.imap(self.new_category, categories):
+                subcats = self.create_subcategories(cat, subcategory_num)
+                print(f"Created subcategories: {subcats} for category {cat}")
+                scats = [(c, cat_id) for c in subcats]
+                for scat, scat_id in pool.starmap(self.new_category, scats):
+                    print(f"Adding articles to subcategory {scat_id}: {scat} scat_json['link']")
 
-            start_date = self.publish_articles(cat, article_num, header_num, start_date, days_delta, forward_delta, cat_id)
-            subcats = self.create_subcategories(cat, subcategory_num)
-            print(f"Created subcategories: {subcats}")
-            for scat in subcats:
-                scat_desc = self.write_cat_description(scat)
-                scat_json = self.create_category(scat, scat_desc, cat_id)
-                scat_id = scat_json['id']
-                print(f"Adding articles to subcategory {scat_id}: {scat_json['link']}")
 
-                start_date = self.publish_articles(scat, article_num, header_num, start_date, days_delta, forward_delta, scat_id)
+    def populate_structure(self, 
+                         article_num:int, 
+                         header_num:int,
+                         start_date: datetime = datetime.now(), 
+                         days_delta:int = 7, 
+                         forward_delta:bool = True
+                         ):
+        #set publication time & delay parameters
+        self.start_date = start_date
+        self.days_delta = days_delta
+        self.forward_delta = forward_delta
+        #get categories from WP API
+        categories = self.get_categories()
+        #create data tuple for each category
+        data_prime = [(c['name'], article_num, c['id']) for c in categories if c['name'] != "Bez kategorii"]
 
+        #for small articles parallelize writing articles
+        if article_num > header_num:
+            with Pool() as pool:
+                for titles, cat_id in pool.starmap(self.create_titles, data_prime):                
+                    data = [(header_num, t, self.start_date, cat_id) for t in titles]
+                    if article_num > 5:
+                        pool.starmap(self.create_article, data)
+                    else:
+                        for d in data:
+                            self.create_article(*d)
+        #for big articles parallelize writing sections
+        else:
+            for d in data_prime:
+                titles, cat_id = self.create_titles(*d)
+                for t in titles:
+                    self.create_article(header_num, t, self.start_date, cat_id, parallel=True)
 
 
 if __name__ == "__main__":
