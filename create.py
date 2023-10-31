778aeb1a561c5bf163a54843c8be84f29606fe39
RedHot099 <redhotdevil996@gmail.com>
2023-10-31 08:35:18 +0100

    Update site creation

--- create.py
+++ create.py
@@ -47,9 +47,8 @@ class Create:
 		print("Logging into panel")
 		self.driver.get(f"{self.panel}/")
 		# sleep(1)
-		WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, "input")))
-		self.driver.find_element("id", "username").find_element(By.TAG_NAME, "input").send_keys(self.user)
-		self.driver.find_element("id", "password").find_element(By.TAG_NAME, "input").send_keys(self.password)
+		WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#username"))).send_keys(self.user)
+		WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#password"))).send_keys(self.password)
 		self.driver.execute_script("document.getElementsByTagName('button')[0].click()")
 		sleep(1)
 		return self.driver
@@ -59,8 +58,8 @@ class Create:
 		self.driver.execute_script(f"window.open('{self.panel}/user/domains/add-domain');")
 		sleep(0.2)
 		self.driver.switch_to.window(self.driver.window_handles[1])
-		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.input-text.grow.pr-12")))
-		self.driver.find_elements(By.CSS_SELECTOR, "input.input-text.grow.pr-12")[0].send_keys(name)
+		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.input-text.grow")))
+		self.driver.find_elements(By.CSS_SELECTOR, "input.input-text.grow")[0].send_keys(name)
 		sleep(2)
 		WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.-theme-primary.-size-big")))
 		self.driver.find_element(By.CSS_SELECTOR, "button.-theme-primary.-size-big").click()
@@ -108,9 +107,7 @@ class Create:
 		print("DataBase")
 		#DB
 		self.driver.get(f'{self.panel}/user/database/create')
-		# sleep(1)
-		WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.TAG_NAME, "input")))
-		self.driver.find_elements(By.TAG_NAME, "input")[1].send_keys(name.partition('.')[0])
+		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.input-text"))).send_keys(name.partition('.')[0])
 		WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.inputPassword>div>div>div>button"))).click()
 		sleep(0.5)
 		WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.-theme-safe.-size-big"))).click()
@@ -118,8 +115,9 @@ class Create:
 		#Copy db creds
 		sleep(2)
 		db_creds = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "dialog-content-wrapper"))).get_attribute("innerText").split('\n')
-		db_user = db_creds[3].split('\t')[1]
-		db_pass = db_creds[4].split('\t')[1]
+		db_creds = list(filter(None, db_creds))
+		db_user = db_creds[-2].split('\t')[-1]
+		db_pass = db_creds[-1].split('\t')[-1]
 
 		print(f"{db_user} : {db_pass}")
 		return db_user, db_pass
@@ -129,16 +127,25 @@ class Create:
 		print("FTP")
 		#FTP
 		self.driver.get(f'{self.panel}/user/ftp-accounts/create')
+		#change domain
+		if(not self.panel.endswith(WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span.domain"))).get_attribute("innerText"))):
+			print("Wrong domain: "+ self.driver.find_element(By.CSS_SELECTOR, "span.domain").get_attribute("innerText"))
+			self.driver.find_element(By.CSS_SELECTOR, "span.domain").click()
+			for a in self.driver.find_elements(By.CSS_SELECTOR, "a#refreshed-domain-select-dropdown-item"):
+				if self.panel.endswith(a.get_attribute("innerText")):
+					a.click()
+					break
 		# sleep(1)
-		WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.TAG_NAME, "input")))
-		self.driver.find_elements(By.TAG_NAME, "input")[1].send_keys("admin")
+		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.input-text"))).send_keys("admin")
 		WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.inputPassword>div>div>div>button"))).click()
-		sleep(0.5)
-		WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.-theme-safe.-size-big"))).click()
+		sleep(1.5)
+		WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.button.-theme-safe.-size-big"))).click()
 
 		#Copy ftp creds
 		sleep(1)
-		ftp_creds = self.driver.find_element(By.CLASS_NAME, "dialog-content-wrapper").get_attribute("innerText").split('\n')
+		print(self.driver.current_url)
+		ftp_creds = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "dialog-content-wrapper"))).get_attribute("innerText").split('\n')
+		ftp_creds = list(filter(None, ftp_creds))
 		ftp_user = ftp_creds[0].split('\t')[1]
 		ftp_pass = ftp_creds[1].split('\t')[1]
 
